d6546359ac58768cf02f5da3a84b46d00edfcbdc
Mohamed-ncib19 <98429286+Mohamed-ncib19@users.noreply.github.com>
2022-10-21 15:40:58 +0200

    Add files via upload
    A little funny game Rock Paper Scissors built with simple HTML-CSS-JS

---
+++ js/index.js
@@ -0,0 +1,35 @@
+let choices = document.querySelectorAll('.choice');
+let botChoices = ['rock','papaer','scissors']; 
+
+//random number for bot choices
+let generateRandomNumber=()=>{
+    return Math.floor(Math.random()*3);
+}
+
+let winner= function (x,y){
+    return (
+        x=="paper" && y=="rock" ? "you won"
+        :x=="paper" && y=="scissors" ? "you lost"
+        :x=="rock" && y=="paper" ? "you lost"
+        :x=="rock" && y=="scissors" ? "you won"
+        :x=="scissors" && y=="rock" ? "you lost"
+        :x=="scissors" && y=="paper" ? "you won"
+        :"draw"
+    )
+}
+
+//bot choices
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        let user = choice.getAttribute("data-option");
+        let bot  = botChoices[generateRandomNumber()];
+        console.log(winner(user,bot));
+        
+        document.getElementById('demo').innerHTML="the bot palys " +bot+"<br>"+winner(user,bot);
+       
+       
+    })
+});    
+
+
